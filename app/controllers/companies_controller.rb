f9b7b8f00cdcddf1d23998c8b9596c90dcb59fb4
Thiago <tlcrezende@gmail.com>
2023-01-04 13:50:56 -0300

    Add controllers companies, applicants and positions

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,14 @@
+class CompaniesController < ApplicationController
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'applicants/index'
+  get 'applicants/new'
+  get 'positions/index'
+  get 'positions/new'
+  get 'positions/edit'
+  get 'positions/show'
+  get 'companies/new'
+  get 'companies/edit'
   get 'home/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
