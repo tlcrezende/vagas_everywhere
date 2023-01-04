13156a4efa24f18aa04445891e9890dc90f34c56
Thiago <tlcrezende@gmail.com>
2023-01-04 10:49:26 -0300

    Add config i18n and time_zone

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,13 @@ require "action_cable/engine"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+def set_locate_configs_and_timezone
+  config.time_zone= 'Brasilia'
+  config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**/*.{rb,yml}').to_s]
+  config.i18n.available_locales = ['pt-BR']
+  config.i18n.default_locale = 'pt-BR'
+end
+
 module VagaEverywhere
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
