d059dc426243e299129d38a6254b8ee0d08b5162
Thiago <tlcrezende@gmail.com>
2023-01-04 13:54:11 -0300

    Add routes w/ resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,10 @@
 Rails.application.routes.draw do
-  get 'applicants/index'
-  get 'applicants/new'
-  get 'positions/index'
-  get 'positions/new'
-  get 'positions/edit'
-  get 'positions/show'
-  get 'companies/new'
-  get 'companies/edit'
-  get 'home/index'
+  resources :companies, only: [:new, :edit, :update, :create]
+  resources :positions
+  resources :applicants, only: [:index, :new, :create]
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#index"
+  root "home#index" 
 end
