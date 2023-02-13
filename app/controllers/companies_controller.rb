b6f48555afb6b925a7f135ba2ec681fdffc5b9ec
Thiago <tlcrezende@gmail.com>
2023-02-13 01:37:07 -0300

    add endpoint create

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,14 +1,16 @@
 class CompaniesController < ApplicationController
   def new
+    @company = current_user.build_company
   end
 
   def edit
   end
 
   def create
+    render json: {}, status: :unprocessable_entity
   end
 
   def update
   end
-  
+
 end
